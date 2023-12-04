3f0d1d2fba636b16f440373e6a7df2c4c490a449
LeeSPyo <oco141@naver.com>
2023-12-04 09:05:14 +0900

    테스트 - 데이터베이스 연동

--- itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import javax.sql.DataSource;
 
-
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
