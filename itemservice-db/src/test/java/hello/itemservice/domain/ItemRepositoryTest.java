ca895c8bd605c38e909a506bb8aa039a44207d73
LeeSPyo <oco141@naver.com>
2023-12-04 09:41:03 +0900

    테스트 - @Transactional

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,35 +5,34 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 
+@Commit
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
 	@Autowired
 	ItemRepository itemRepository;
-
-	// 트랜잭션 관련 코드
-	@Autowired
-	PlatformTransactionManager transactionManager;
-	TransactionStatus status;
-
-	@BeforeEach
-	void beforeEach() {
-		// 트랜잭션 시작
-		status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-	}
+	
+	/*
+	 * // 트랜잭션 관련 코드
+	 * 
+	 * @Autowired PlatformTransactionManager transactionManager; TransactionStatus
+	 * status;
+	 * 
+	 * @BeforeEach void beforeEach() { // 트랜잭션 시작 status =
+	 * transactionManager.getTransaction(new DefaultTransactionDefinition()); }
+	 */
 
 	@AfterEach
 	void afterEach() {
@@ -42,7 +41,7 @@ class ItemRepositoryTest {
 			((MemoryItemRepository) itemRepository).clearStore();
 		}
 		//트랜잭션 롤백
-		transactionManager.rollback(status);
+		//transactionManager.rollback(status);
 	}
 
 	@Test
